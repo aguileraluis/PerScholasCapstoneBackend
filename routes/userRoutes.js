2e3b7cad2b7b93b3869deca481d3944d2e34fb34
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-12-13 13:56:15 -0500

    finished more routes and user controller functions

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,24 +1,25 @@
 import express from "express"; 
 // import { isAdminRoute, protectRoute } from "../middlewares/authMiddleware";
-import { registerUser } from "../controllers/userController.js";
+import { activateUserProfile, changeUserPassword, deleteUserProfile, getNotificationsList, getTeamList, loginUser, logoutUser, markNotificationRead, registerUser } from "../controllers/userController.js";
 
 const router = express.Router(); 
 
 router.post("/register", registerUser); 
 router.post("/login", loginUser); 
-// router.post("/logout", logoutUser); 
+router.post("/logout", logoutUser); 
 
-// router.get("/get-team", protectRoute, isAdminRoute, getTeamList); 
-// router.get("/notifications", protectRoute, getNotificationsList);
+router.get("/get-team", protectRoute, isAdminRoute, getTeamList); 
+router.get("/notifications", protectRoute, getNotificationsList);
 
-// router.put("/profile", protectRoute, updateUserProfile);
-// router.put("/read-noti", protectRoute, markNotificationRead); 
-// router.put("/change-password", protectRoute, changeUserPassword); 
+router.put("/profile", protectRoute, updateUserProfile);
+router.put("/read-noti", protectRoute, markNotificationRead); 
+router.put("/change-password", protectRoute, changeUserPassword); 
 
-// router
-//   .route("/:id")
-//   .put(protectRoute, isAdminRoute, activateUserProfile)
-//   .delete(protectRoute, isAdminRoute, deleteUserProfile)
+// For Admin Only - Admin Routes
+router
+  .route("/:id")
+  .put(protectRoute, isAdminRoute, activateUserProfile)
+  .delete(protectRoute, isAdminRoute, deleteUserProfile)
 
 
 export default router; 
