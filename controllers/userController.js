31c9a2aeb8dcd7205ff5e68efcbda46a8e34727b
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-12-16 18:37:03 -0500

    finished teams functionality and database routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,251 +1,252 @@
+import asyncHandler from "express-async-handler";
+import Notice from "../models/notification.js";
 import User from "../models/user.js";
-import { response } from "express"; 
-import { createJWT } from "../utils/index.js"; 
-import Notice from "../models/notification.js"; 
+import createJWT from "../utils/index.js";
 
-export const registerUser = async (req, res) => {
-  try {
-    const { name, email, password, isAdmin, role, title } = req.body;
+// POST request - login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    const userExists = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (userExists) {
-      return res.status(400).json({
-        status: false,
-        message: "User already exists",
-      });
-    }
+  if (!user) {
+    return res
+      .status(401)
+      .json({ status: false, message: "Invalid email or password." });
+  }
 
-    const user = await User.create({
-      name,
-      email,
-      password,
-      isAdmin,
-      role,
-      title,
+  if (!user?.isActive) {
+    return res.status(401).json({
+      status: false,
+      message: "User account has been deactivated, contact the administrator",
     });
-
-    if(user) {
-      isAdmin ? createJWT(res, user._id) : null; 
-
-      user.password = undefined; 
-
-      res.status(201).json({
-        status: true, 
-        message: "User registration successful", 
-        user
-      });  
-    } else {
-      return res 
-        .status(400)
-        .json({ status: false, message: "Invalid user data" }); 
-    }
-  } catch (error) {
-    return res.status(400).json({ status: false, message: error.message });
   }
+
+  const isMatch = await user.matchPassword(password);
+
+  if (user && isMatch) {
+    createJWT(res, user._id);
+
+    user.password = undefined;
+
+    res.status(200).json(user);
+  } else {
+    return res
+      .status(401)
+      .json({ status: false, message: "Invalid email or password" });
+  }
+});
+
+// POST - Register a new user
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, isAdmin, role, title } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ status: false, message: "Email address already exists" });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    isAdmin,
+    role,
+    title,
+  });
+
+  if (user) {
+    isAdmin ? createJWT(res, user._id) : null;
+
+    user.password = undefined;
+
+    res.status(201).json(user);
+  } else {
+    return res
+      .status(400)
+      .json({ status: false, message: "Invalid user data" });
+  }
+});
+
+// POST -  Logout user / clear cookie
+const logoutUser = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 };
 
-export const loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body; 
+const getTeamList = asyncHandler(async (req, res) => {
+  const { search } = req.query;
+  let query = {};
 
-    const user = await User.findOne({ email }); 
-
-    if (!user) {
-      return res
-        .status(401)
-        .json({ status: false, message: "Invalid email or password." }); 
-    }
-
-    if (!user?.isActive) {
-      return res.status(401).json({
-        status: false, 
-        message: 
-          "User account has been deactivated, contact the administrator"
-      });
-    }
-
-    const isMatch = await user.matchPassword(password); 
-
-    if (user && isMatch) {
-      createJWT(res, user._id)
-
-      user.password = undefined; 
-      
-      res.status(200).json(user)
-    } else {
-      return res
-        .status(401)
-        .json({ status: false, message: "Invalid email or password" })
-    }
-  } catch (error) {
-    return res.status(400).json({ status: false, message: error.message });
+  if (search) {
+    const searchQuery = {
+      $or: [
+        { title: { $regex: search, $options: "i" } },
+        { name: { $regex: search, $options: "i" } },
+        { role: { $regex: search, $options: "i" } },
+        { email: { $regex: search, $options: "i" } },
+      ],
+    };
+    query = { ...query, ...searchQuery };
   }
-}; 
 
-export const logoutUser = async (req, res) => {
+  const user = await User.find(query).select("name title role email isActive");
+
+  res.status(201).json(user);
+});
+
+// @GET  - get user notifications
+const getNotificationsList = asyncHandler(async (req, res) => {
+  const { userId } = req.user;
+
+  const notice = await Notice.find({
+    team: userId,
+    isRead: { $nin: [userId] },
+  })
+    .populate("task", "title")
+    .sort({ _id: -1 });
+
+  res.status(200).json(notice);
+});
+
+// @GET  - get user task status
+const getUserTaskStatus = asyncHandler(async (req, res) => {
+  const tasks = await User.find()
+    .populate("tasks", "title stage")
+    .sort({ _id: -1 });
+
+  res.status(200).json(tasks);
+});
+
+// @GET  - get user notifications
+const markNotificationRead = asyncHandler(async (req, res) => {
   try {
-    res.cookie("token", "", {
-      httpOnly: true, 
-      expires: new Date(0)
-    });
+    const { userId } = req.user;
+    const { isReadType, id } = req.query;
 
-    res.status(200).json({ message: "Logout successful" })
-  } catch (error) {
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const getTeamList = async (req, res) => {
-  try {
-    const users = await User.find().select("name title role email isActive"); 
-
-    res.status(200).json(users); 
-  } catch (error) {
-    return res.status(400).json({ status: false, messag: error.message }); 
-  }
-};
-
-export const getNotificationsList = async (req, res) => {
-  try {
-    const { userId } = req.user; 
-
-    const notice = await Notice.findOne({
-      team: userId, 
-      isRead: { $nin: [userId] }
-    }).populate("task", "title"); 
-
-    res.status(201).json(notice); 
-  } catch (error) {
-    console.log(error); 
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const updateUserProfile = async (req, res) => {
-  try {
-    const { userId, isAdmin } = req.user; 
-    const { _id } = req.body; 
-
-    const id = isAdmin && userId === _id ? userId : isAdmin && userId !== _id ? _id : userId; 
-
-    const user = await User.findById(id); 
-
-    if (user) {
-      user.name = req.body.name || user.name; 
-      user.title = req.body.title || user.title; 
-      user.role = req.body.role || user.role; 
-
-      const updatedUser = await user.save(); 
-
-      user.password = undefined; 
-
-      res.status(201).json({
-        status: true, 
-        message: "Profile Updated Successfully.", 
-        user: updatedUser
-      })
-    } else {
-      res.status(404).json({ status: false, message: "User not found" });
-    }
-  } catch (error) {
-    console.log(error); 
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const markNotificationRead = async (req, res) => {
-  try {
-    const { userId } = req.user; 
-
-    const { isReadType, id } = req.query; 
-
-    if (isReadType == "all") {
+    if (isReadType === "all") {
       await Notice.updateMany(
-        { team: userId, isRead: { $nin: [userId] }}, 
-        { $push: {isRead: userId} }, 
+        { team: userId, isRead: { $nin: [userId] } },
+        { $push: { isRead: userId } },
         { new: true }
-    );
+      );
     } else {
       await Notice.findOneAndUpdate(
-        { _id: id, isRead: { $nin: [userId] }}, 
-        { $push : { isRead: userId }}, 
+        { _id: id, isRead: { $nin: [userId] } },
+        { $push: { isRead: userId } },
         { new: true }
-      )
+      );
     }
-
     res.status(201).json({ status: true, message: "Done" });
   } catch (error) {
-    console.log(error); 
-    return res.status(400).json({ status: false, message: error.message });
+    console.log(error);
   }
-};
+});
 
-export const changeUserPassword = async (req, res) => {
-  try {
-    const { userId } = req.user; 
+// PUT - Update user profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const { userId, isAdmin } = req.user;
+  const { _id } = req.body;
 
-    const user = await User.findById(userId); 
+  const id =
+    isAdmin && userId === _id
+      ? userId
+      : isAdmin && userId !== _id
+      ? _id
+      : userId;
 
-    if (user) {
-      user.password = req.body.password; 
+  const user = await User.findById(id);
 
-      await user.save(); 
+  if (user) {
+    user.name = req.body.name || user.name;
+    // user.email = req.body.email || user.email;
+    user.title = req.body.title || user.title;
+    user.role = req.body.role || user.role;
 
-      user.password = undefined; 
+    const updatedUser = await user.save();
 
-      res.status(201).json({
-        status: true, 
-        message: `Password changed successfully.`,
-      })
-    }
-  } catch (error) {
-    console.log(error); 
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const activateUserProfile = async (req, res) => {
-  try {
-   const { id } = req.params; 
-   const user = await User.findById(id); 
-
-   if (user) {
-    user.isActive = req.body.isActive; 
-    
-    await user.save(); 
-
-    user.password = undefined; 
+    user.password = undefined;
 
     res.status(201).json({
-      status: true, 
+      status: true,
+      message: "Profile Updated Successfully.",
+      user: updatedUser,
+    });
+  } else {
+    res.status(404).json({ status: false, message: "User not found" });
+  }
+});
+
+// PUT - active/disactivate user profile
+const activateUserProfile = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findById(id);
+
+  if (user) {
+    user.isActive = req.body.isActive;
+
+    await user.save();
+
+    user.password = undefined;
+
+    res.status(201).json({
+      status: true,
       message: `User account has been ${
         user?.isActive ? "activated" : "disabled"
-      }`
-    })
-   } else {
-    res.status(404).json({ status: false, message: "User not found" }); 
-   }
-    
-  } catch (error) {
-    console.log(error); 
-    return res.status(400).json({ status: false, message: error.message });
+      }`,
+    });
+  } else {
+    res.status(404).json({ status: false, message: "User not found" });
   }
-};
+});
 
-export const deleteUserProfile = async (req, res) => {
-  try {
-    const { id } = req.params; 
+const changeUserPassword = asyncHandler(async (req, res) => {
+  const { userId } = req.user;
 
-    await User.findByIdAndDelete(id); 
+  const user = await User.findById(userId);
 
-    res 
-      .status(200)
-      .json({ status: true, messag: "User deleted successfully" }); 
-  } catch (error) {
-    console.log(error); 
-    return res.status(400).json({ status: false, message: error.message });
+  if (user) {
+    user.password = req.body.password;
+
+    await user.save();
+
+    user.password = undefined;
+
+    res.status(201).json({
+      status: true,
+      message: `Password chnaged successfully.`,
+    });
+  } else {
+    res.status(404).json({ status: false, message: "User not found" });
   }
+});
+
+// DELETE - delete user account
+const deleteUserProfile = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  await User.findByIdAndDelete(id);
+
+  res.status(200).json({ status: true, message: "User deleted successfully" });
+});
+
+export {
+  activateUserProfile,
+  changeUserPassword,
+  deleteUserProfile,
+  getNotificationsList,
+  getTeamList,
+  getUserTaskStatus,
+  loginUser,
+  logoutUser,
+  markNotificationRead,
+  registerUser,
+  updateUserProfile,
 };
-
-
